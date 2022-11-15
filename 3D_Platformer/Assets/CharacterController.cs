c0f0e65fecd68259583a05c5b1209840ab182291
hgunstone <hgunstone2022@elam.co.uk>
2022-11-15 11:14:52 +0000

    Physics controller/ jump

--- 3D_Platformer/Assets/CharacterController.cs
+++ 3D_Platformer/Assets/CharacterController.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    float maxSpeed = 0.1f;
+    float maxSpeed = 35.0f;
     float rotation = 0.0f;
     float camRotation = 0.0f;
     float rotationSpeed = 2.0f;
     float camRotationSpeed = 1.5f;
     GameObject cam;
+    Rigidbody myRigidbody;
 
-
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f
 
     void Start()
     {
@@ -20,14 +24,17 @@ public class CharacterController : MonoBehaviour
 
     void Update()
     {
-        transform.position = transform.position + transform.forward * (Input.GetAxis("Vertical") * maxSpeed);
-        
+        myRigidbody = GetComponent<Rigidbody>();
+
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
+
         rotation = rotation + Input.GetAxis("Mouse X");
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
         
         camRotation = camRotation + Input.GetAxis("Mouse Y");
         cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
-             
 
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
     }
 }
