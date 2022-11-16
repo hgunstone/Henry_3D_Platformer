5756842921f57c9360e71237bf4c7a9ef9dc94ff
hgunstone <hgunstone2022@elam.co.uk>
2022-11-16 11:29:06 +0000

    more character controller

--- 3D_Platformer/Assets/ThirdPersonCam.cs
+++ 3D_Platformer/Assets/ThirdPersonCam.cs
@@ -22,9 +22,13 @@ public class ThirdPersonCam : MonoBehaviour
         Vector3 veiwDir = player.position - new Vector3(transfrom.x, playerposition.y, transform.position.z);
         orienatation.forward = ViewDir.normalized;
 
-        float horizontalInput = Input.GetAxis("Horizontal")
-        float verticalInput = Input.GetAxis("Vertical")
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
         Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if(inputDir != Vector3 .zero)
+            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            
 
     }
 }
